a3b08236bf16f0bc43a48c32b6bfecbc10c49224
Martim Mourão <martimmourao@gmail.com>
2026-02-23 15:54:52 +0000

    fix: media volumes

--- project/settings.py
+++ project/settings.py
@@ -216,9 +216,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_ROOT = "app/media"
+MEDIA_ROOT = "/app/media"
 
 STATIC_URL = '/static/'
 
