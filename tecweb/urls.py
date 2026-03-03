af8c436afe5b46af389673334ce0259d42a810e6
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2026-03-03 23:25:41 +0100

    Add download media path to URLs

--- tecweb/urls.py
+++ tecweb/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
     path('sessoes/orador/', views.sessoes_orador, name='sessoes_orador'),
     path("inquerito/<int:inscricao_id>/", views.inquerito_view, name="inquerito"),
     # path('feedback-numerico/', views.feedback_numerico_view, name='feedback_numerico'),
+    
+    path('download-media/', views.zip_media, name='download_media'),
 ]
